c053a94ec62abda8d69c27bcbfe5394bfdd5e7e8
leegaon <gaon1970@naver.com>
2025-03-30 17:08:27 +0900

    git commit test

--- src/main/java/org/boot/controller/UserController.java
+++ src/main/java/org/boot/controller/UserController.java
@@ -5,20 +5,16 @@ import org.boot.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-@Controller
+@RestController
+@RequestMapping("/api/users")
 public class UserController {
 
     @Autowired
     private UserService userService;
 
-    @GetMapping("/api/users/signup")
-    public String showSignUpPage() {
-        return "signupPage";
-    }
-    @GetMapping("/api/users/check-id")
+    @GetMapping("/check-id")
     public ResponseEntity<CheckIdResponse> checkUserId(@RequestParam String userId) {
         boolean exists = userService.isUserIdExists(userId);
         CheckIdResponse response = new CheckIdResponse(!exists, exists ? "userId already exists." : "You can use this userId.");
@@ -28,16 +24,19 @@ public class UserController {
             return ResponseEntity.ok(response); // 200
         }
     }
-    @PostMapping("/api/users/register")
-    public ResponseEntity<RegisterResponse> register(@RequestBody RegisterRequest registerRequest) {
-        // 디버깅을 위한 로그 추가
-        System.out.println("Received Password: " + registerRequest.getUserPassword());
-        System.out.println("Received Password Confirm: " + registerRequest.getUserPasswordConfirm());
 
+    @PostMapping("/register")
+    public ResponseEntity<RegisterResponse> register(@RequestBody RegisterRequest registerRequest) {
         // pw & pw 확인 일치/불일치 체크
         if (!registerRequest.getUserPassword().equals(registerRequest.getUserPasswordConfirm())) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST)
                     .body(new RegisterResponse(false, "Passwords do not match.")); // 400
+        }
+
+        // id 중복 체크
+        if (userService.isUserIdExists(registerRequest.getUserId())) {
+            return ResponseEntity.status(HttpStatus.CONFLICT)
+                    .body(new RegisterResponse(false, "userId already exists.")); // 409
         }
 
         // RegisterRequest -> UserDTO 로 변환
@@ -46,26 +45,12 @@ public class UserController {
         userDTO.setUserPassword(registerRequest.getUserPassword());
         userDTO.setUserName(registerRequest.getUserName());
 
-        // id 중복 체크
-        if (userService.isUserIdExists(userDTO.getUserId())) {
-            return ResponseEntity.status(HttpStatus.CONFLICT)
-                    .body(new RegisterResponse(false, "userId already exists.")); // 409
-        }
-
         // 회원가입 성공 시
         RegisterResponse response = userService.register(userDTO);
         return ResponseEntity.ok(response); // 200
     }
 
-    @GetMapping("/")
-    public String redirectToLogin() {
-        return "loginPage";
-    }
-    @GetMapping("/api/users/login")
-    public String showLoginPage() {
-        return "loginPage";
-    }
-    @PostMapping("/api/users/login")
+    @PostMapping("/login")
     public ResponseEntity<LoginResponse> login(@RequestBody LoginRequest loginRequest) {
         String userId = loginRequest.getUserId();
         String userPassword = loginRequest.getUserPassword();
